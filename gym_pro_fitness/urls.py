7920cbfa8bc394a632eb62ab4d58a6982d974510
GeorgeH23 <>
2023-11-12 02:20:54 +0000

    Create the skeleton of the Home Page #11

--- gym_pro_fitness/urls.py
+++ gym_pro_fitness/urls.py
@@ -15,7 +15,9 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path
+from workouts import views
 
 urlpatterns = [
     path('admin/', admin.site.urls),
+    path('', views.get_home_page, name='get_home_page'),
 ]
