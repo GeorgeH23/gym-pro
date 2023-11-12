880c744ae1b572e432a91832c445679afd405d8a
GeorgeH23 <>
2023-11-12 13:32:52 +0000

    Fix the Heroku deployment #11

--- gym_pro_fitness/settings.py
+++ gym_pro_fitness/settings.py
@@ -28,8 +28,10 @@ TEMPLATES_DIR = os.path.join(BASE_DIR, 'templates')
 # SECURITY WARNING: keep the secret key used in production secret!
 SECRET_KEY = os.environ.get('SECRET_KEY')
 
+X_FRAME_OPTIONS = 'SAMEORIGIN'
+
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
 ALLOWED_HOSTS = ['gym-pro-fitness-d84818eee5ff.herokuapp.com', 'localhost',
                  '8000-georgeh23-gym-pro-fitnes-4zxcif5lyi.us2.codeanyapp.com']
