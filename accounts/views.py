eb0a9fae7b2fff6666e7bed1ceb52460d007dc84
GeorgeH23 <>
2023-11-13 06:30:43 +0000

    Create the post request for registration #3

--- accounts/views.py
+++ accounts/views.py
@@ -1,3 +1,24 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
+from django.contrib.auth.forms import UserCreationForm
+from django.contrib.auth import authenticate, login
+from django.contrib import messages
+from .forms import RegistrationForm
+from .models import UserProfile
 
-# Create your views here.
+
+# User registration view
+def register_user(request):
+    if request.method == 'POST':
+        form = RegistrationForm(request.POST)
+
+        form.is_valid()
+        user = form.save()
+        username = form.cleaned_data.get('username')
+        password = form.cleaned_data.get('password1')
+        form = authenticate(username=username, password=password)
+        login(request, user)
+    else:
+        form = RegistrationForm()
+
+    context = {'form': form}
+    return render(request, 'authentication/register.html', context)
