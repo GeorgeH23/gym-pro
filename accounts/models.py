f24fd369a03e0252c501c4d09136afbc2b6d9efe
GeorgeH23 <>
2023-11-13 06:02:45 +0000

    Create the user profile model #3

--- accounts/models.py
+++ accounts/models.py
@@ -1,3 +1,14 @@
 from django.db import models
+from django.contrib.auth.models import User
+from cloudinary.models import CloudinaryField
 
-# Create your models here.
+
+# User Profile Model
+class UserProfile(models.Model):
+    user = models.OneToOneField(User, on_delete=models.CASCADE)
+    profile_picture = CloudinaryField(
+        'Profile Picture', default='profile_picture')
+
+    def __str__(self):
+        username = getattr(self.user, 'username', 'NoUsername')
+        return 'Profile: {}'.format(username)
