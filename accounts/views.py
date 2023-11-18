b4e1a52ac178f3e56febf699ce61ab3b6cefc19d
GeorgeH23 <gheorghita.harpa0423@yahoo.com>
2023-11-18 22:08:57 +0000

    Complete the request and the template of the login #16

--- accounts/views.py
+++ accounts/views.py
@@ -1,9 +1,8 @@
 from django.shortcuts import render, redirect
-from django.contrib.auth.forms import UserCreationForm
+from django.contrib.auth.models import User
 from django.contrib.auth import authenticate, login
 from django.contrib import messages
 from .forms import RegistrationForm
-from .models import UserProfile
 
 
 # User registration view
@@ -36,10 +35,52 @@ def register_user(request):
     return render(request, 'authentication/register.html', context)
 
 
+# User login view
 def login_user(request):
     if request.method == 'POST':
         identifier = request.POST["username_or_email"]
         password = request.POST["password"]
 
+        # Check if the identifier is an email
+        if '@' in identifier:
+            user = User.objects.filter(email=identifier).first()
+        # Check if the identifier is a username
+        elif '@' not in identifier:
+            user = User.objects.filter(username=identifier).first()
+        # Check if the identifier doesn't exist
+        else:
+            try:
+                user = User.objects.get(username=identifier)
+            except User.DoesNotExist:
+                user = None
+
+        if user:
+            form = authenticate(
+                request, username=identifier, password=password)
+            if form is not None:
+                login(request, form)
+                messages.success(
+                    request,
+                    'Login successful.',
+                    extra_tags='success'
+                )
+                # Redirect to home page after successful login
+                return redirect('/')
+            else:
+                messages.error(
+                    request,
+                    'Invalid login credentials.',
+                    extra_tags='danger'
+                )
+                # Redirect to login page if credentials are invalid
+                return redirect('login_user')
+        else:
+            messages.error(
+                request,
+                'User does not exist.',
+                extra_tags='danger'
+            )
+            # Redirect to registration page if user doesn't exist
+            return redirect('register_user')
     else:
         return render(request, 'authentication/login.html', {})
