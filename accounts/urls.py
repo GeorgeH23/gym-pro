1922a2e4c1d433570586730775c67b77d2fe2c32
GeorgeH23 <gheorghita.harpa0423@yahoo.com>
2023-11-22 13:30:19 +0000

    Add url path for delete #9

--- accounts/urls.py
+++ accounts/urls.py
@@ -7,4 +7,5 @@ urlpatterns = [
     path('logout_user', views.logout_user, name='logout_user'),
     path('profile/', views.user_profile, name='user_profile'),
     path('profile/update/', views.update_profile, name='update_profile'),
+    path('profile/delete/', views.delete_account, name='delete_account'),
 ]
