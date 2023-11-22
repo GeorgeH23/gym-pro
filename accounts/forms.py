20578f1e003db18bde89a4d884f1f665d0a5cafc
GeorgeH23 <gheorghita.harpa0423@yahoo.com>
2023-11-22 20:21:37 +0000

    Create Contact form #14

--- accounts/forms.py
+++ accounts/forms.py
@@ -139,4 +139,35 @@ class UpdateProfileForm(UserChangeForm):
 
     class Meta:
         model = User
-        fields = ('email', 'profile_image', )
+        fields = ('email', 'profile_image', 'new_password1', 'new_password2',)
+
+
+# Contact Form
+class ContactForm(forms.Form):
+    fullName = forms.CharField(
+        label='',
+        max_length=100,
+        widget=forms.TextInput(attrs={
+            'class': 'form-control',
+            'placeholder': 'Full Name (required)',
+            'id': 'fullName'}
+        ),
+        required=True,
+    )
+    email = forms.EmailField(
+        label='',
+        widget=forms.EmailInput(attrs={
+            'class': 'form-control',
+            'placeholder': 'Email (required)',
+            'id': 'email'}
+        ),
+        required=True,
+    )
+    message = forms.CharField(
+        label='',
+        widget=forms.Textarea(attrs={
+            'class': 'form-control',
+            'placeholder': 'Write your message (required)',
+            'id': 'message'}
+        ),
+    )
