f4cffadae09774a505f6bca7ce670c287e5f3d44
GeorgeH23 <gheorghita.harpa0423@yahoo.com>
2023-11-24 00:07:40 +0000

    Create the type model #19

--- workouts/models.py
+++ workouts/models.py
@@ -1,3 +1,14 @@
 from django.db import models
 
 # Create your models here.
+
+# Type model
+class Type(models.Model):
+    name = models.CharField(max_length=100)
+
+    def __str__(self):
+        return f'{self.name}'
+
+    class Meta:
+        ordering = ['name']
+        verbose_name_plural = 'Tipes'
