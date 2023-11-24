204f87e37d2038be96a4107f5ae66edf52fe19bf
GeorgeH23 <gheorghita.harpa0423@yahoo.com>
2023-11-24 01:30:28 +0000

    Add summernote #19

--- gym_pro_fitness/settings.py
+++ gym_pro_fitness/settings.py
@@ -51,6 +51,7 @@ INSTALLED_APPS = [
     'cloudinary',
     'workouts',
     'accounts',
+    'django_summernote',
 ]
 
 MIDDLEWARE = [
--- workouts/admin.py
+++ workouts/admin.py
@@ -1,3 +1,17 @@
 from django.contrib import admin
+from .models import Workout, Type, Intensity
+from django_summernote.admin import SummernoteModelAdmin
 
-# Register your models here.
+# Register Type Model
+admin.site.register(Type)
+
+# Register Intensity Model
+admin.site.register(Intensity)
+
+@admin.register(Workout)
+class WorkoutAdmin(SummernoteModelAdmin):
+    prepopulated_fields = {'slug': ('title',)}
+    list_filter = ('created_on', 'slug')
+    summernote_fields = ('your_rich_text_field',)
+    list_display = ('title', 'type', 'intensity', 'created_on')
+    search_fields = ['title', 'type', 'intensity']
