d22fb461bc9b4b10174c94b9822218929183dd89
GeorgeH23 <gheorghita.harpa0423@yahoo.com>
2023-11-24 00:12:43 +0000

    Create the request for my workouts page #19

--- workouts/views.py
+++ workouts/views.py
@@ -1,7 +1,22 @@
 from django.shortcuts import render
+from django.contrib.auth.decorators import login_required
+from .models import Workout
 
 # Create your views here.
 
 
 def get_home_page(request):
     return render(request, 'workouts/home.html')
+
+
+# My Workouts
+@login_required
+def user_page(request):
+    user = request.user
+
+    user_workouts = Workout.objects.filter(user_id=user.id)
+    context = {
+        'user': user,
+        'workouts': user_workouts,
+    }
+    return render(request, 'workouts/user_page.html', context=context)
