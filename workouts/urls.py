fd5518bca3be7fca2a3f2d31572a0e380e318284
GeorgeH23 <gheorghita.harpa0423@yahoo.com>
2023-11-24 00:10:34 +0000

    Add url path to the add workout page #19

--- workouts/urls.py
+++ workouts/urls.py
@@ -4,4 +4,6 @@ from . import views
 
 urlpatterns = [
     path('', views.get_home_page, name='get_home_page'),
+    path('user_page/', views.user_page, name='user_page'),
+    path('add_workout/', views.AddWorkoutView.as_view(), name='add_workout'),
 ]
