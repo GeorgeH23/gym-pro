426adf5363d94f04f893a9861e3d31777934c1a3
GeorgeH23 <gheorghita.harpa0423@yahoo.com>
2023-11-24 00:09:49 +0000

    Create the workout model #19

--- workouts/models.py
+++ workouts/models.py
@@ -1,4 +1,6 @@
 from django.db import models
+from django.contrib.auth.models import User
+from cloudinary.models import CloudinaryField
 
 # Create your models here.
 
@@ -25,3 +27,36 @@ class Intensity(models.Model):
         ordering = ['name']
         verbose_name_plural = 'Intensities'
 
+
+# Workout model
+class Workout(models.Model):
+    title = models.CharField(max_length=100, blank=False)
+    type = models.ForeignKey(Type, on_delete=models.CASCADE)
+    description = models.TextField()
+    intensity = models.ForeignKey(Intensity, on_delete=models.CASCADE)
+    burned = models.CharField(max_length=100, blank=False)
+    image_url = CloudinaryField('image', default='default')
+    slug = models.SlugField(max_length=100, unique=True)
+
+    user_id = models.ForeignKey(
+        User,
+        on_delete=models.CASCADE,
+        related_name='workout_details'
+    )
+    likes = models.ManyToManyField(
+        User,
+        related_name='liked_workouts',
+        blank=True
+    )
+    liked_by_user = models.BooleanField(default=False)
+    created_on = models.DateTimeField(auto_now_add=True)
+    updated_on = models.DateTimeField(auto_now=True)
+
+    class Meta:
+        ordering = ['-created_on']
+
+    def likes_counter(self):
+        return self.likes.count()
+
+    def __str__(self):
+        return f'{self.title}'
